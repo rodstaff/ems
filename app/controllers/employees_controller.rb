40a25fd916be63553d754190d3febd7e4cf6eda7
rodk <rod2010rod2010@hotmail.com>
2016-07-16 14:37:38 -0400

    updated files

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,10 +1,40 @@
 class EmployeesController < ApplicationController
 
+  # attr_accessor :employee
+
+  def index
+  	@employees = Employee.all
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
   def new
   end
 
+  # def create
+  #   render plain: params[:employee].inspect
+  # end
+
+  # def create
+  # @employee = Employee.new(params[:employee])
+ 
+  # @employee.save
+  # redirect_to @employee
+  # end
+
+  #Below are modified methods improved for security
   def create
-    render plain: params[:employee].inspect
+    @employee = Employee.new(employee_params)
+ 
+    @employee.save
+    redirect_to @employee
+  end
+ 
+  private
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :email)
   end
 
 end
