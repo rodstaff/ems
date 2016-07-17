4080b1abd03f4a2bb4f3b1fd788ef96e7e265994
rodk <rod2010rod2010@hotmail.com>
2016-07-17 16:49:49 -0400

    corrected error in create action in users_controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160717022301) do
+ActiveRecord::Schema.define(version: 20160717200202) do
 
   create_table "cars", force: :cascade do |t|
     t.string   "model"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20160717022301) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "login"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
