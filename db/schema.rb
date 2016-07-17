43834fe26c9ce8883cd6726f8235f35e61b6b475
rodk <rod2010rod2010@hotmail.com>
2016-07-16 22:20:50 -0400

    created model 'Car'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160716165523) do
+ActiveRecord::Schema.define(version: 20160717021715) do
+
+  create_table "cars", force: :cascade do |t|
+    t.string   "model"
+    t.text     "note"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "employees", force: :cascade do |t|
     t.string   "first_name"
